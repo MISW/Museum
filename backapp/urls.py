28aeb00216c7ad2560b6061500b45e5adf1faae8
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2020-09-13 02:41:26 +0900

    Add views & models of developer-detail & game-detail

--- backapp/urls.py
+++ backapp/urls.py
@@ -6,4 +6,6 @@ app_name = 'backapp'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('developers/', views.DevelopersView.as_view(), name='developers'),
+    path('developer-detail/<int:pk>/', views.DeveloperDetailView.as_view(), name='developer-detail'),
+    path('game-detail/<int:pk>/', views.GameDetalView.as_view(), name='game-detail'),
 ]
