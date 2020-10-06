4685183d7a8c49114ca90a5b9d2e6f880c04f141
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2020-10-06 21:24:58 +0900

    Rewrite customuser model

--- backapp/views.py
+++ backapp/views.py
@@ -4,18 +4,18 @@ from django.views import generic
 from .models import *
 
 # Create your views here.
-class IndexView(generic.ListView):
-    model = gameInf
+class IndexView(generic.TemplateView):
+    #model = gameInf
     template_name = 'index.html'
 
 class DevelopersView(generic.ListView):
-    model = developPartInf
+    #model = developPartInf
     template_name = 'developers.html'
 
 class DeveloperDetailView(generic.DetailView):
-    model = developerInf
+    #model = developerInf
     template_name = 'developer-detail.html'
 
 class GameDetalView(generic.DetailView):
-    model = gameInf
+    #model = gameInf
     template_name = 'game-detail.html'
