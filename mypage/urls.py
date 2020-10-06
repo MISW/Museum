c99a756a85f7fada1433bdb7d874485a11e9dcb0
HarrisonKawagoe3960X <hyzitbios@hotmail.com>
2020-10-07 06:24:07 +0900

    Edit Profile , send forms

--- mypage/urls.py
+++ mypage/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('change', views.MypageChangeView.as_view(), name='change'),
     path('profile', views.MypageProfileView.as_view(), name='profile'),
     path('update_profile', views.UpdateProfile.as_view(), name='update_profile'),
+    path('new_application', views.NewApplication.as_view(), name='new_application')
 ]
