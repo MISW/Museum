a8aa4837dbb9021d7ce95cca7c252c82e93affef
わせりん <62173915+Reyt0m@users.noreply.github.com>
2020-10-29 00:16:24 +0900

    ファイル破損回避用

--- misw_gamecenter/urls.py
+++ misw_gamecenter/urls.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-"""misw_gamecenter URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path,re_path
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
 """misw_gamecenter URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -53,4 +29,3 @@ urlpatterns = [
 ]
 
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
->>>>>>> 11302c4c05fa880201ce46afb9e9b8369cfaa17e
