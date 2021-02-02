f76ba75125906ac24e4c3e7d9a694efc09559557
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2021-02-03 04:35:55 +0900

    add: development update view

--- backend/mypage/views.py
+++ backend/mypage/views.py
@@ -146,30 +146,107 @@ class DevelopmentUpdateView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'mypage/developments/update.html'
 
     def get(self, *args, **kwargs):
-        form = DevelopmentUpdateForm()
-
         development = Development.objects.get(pk=self.kwargs['pk'])
-        form.fields['title'].initial = development.title
-        form.fields['description'].initial = development.description
+        media = development.medias.first()
+        media_flag = bool(media)
+        link = development.links.first()
+        link_flag = bool(link)
+        development_form_initial = {
+            'title': development.title,
+            'description': development.description,
+            'associations': development.get_associations(),
+            'is_private': development.is_private,
+            'top_image': [ development.top_image if development.top_image else None]
+        }
+        if media_flag:
+            media_form_initial = {
+                'type': media.type,
+                'file': media.file
+            }
+        else:
+            media_form_initial = {}
 
-        context = dict(form=form)
-        context['development'] = development
+        if link_flag:
+            link_form_initial = {
+                'type': link.type,
+                'link': link.link
+            }
+        else:
+            link_form_initial = {}
+
+        development_form = DevelopmentCreateForm(initial=development_form_initial)
+        media_form = MediaCreateForm(initial=media_form_initial)
+        link_form = LinkCreateForm(initial=link_form_initial)
+        development_form.fields['co_developers'].queryset = (
+            User.objects.filter(is_superuser=False)
+                .exclude(pk=self.request.user.pk)
+        )
+        development_form.fields['co_developers'].initial = (
+            [co_developer.pk for co_developer in development.co_developers.all()]
+        )
+
+        context = {
+            'development': development,
+            'development_form': development_form,
+            'media_form': media_form,
+            'link_form': link_form
+        }
 
         return self.render_to_response(context)
 
     def post(self, request, *args, **kwargs):
         development = Development.objects.get(pk=self.kwargs['pk'])
+        media = development.medias.first()
+        link = development.links.first()
 
         if request.method == 'POST':
-            form = DevelopmentUpdateForm(request.POST, request.FILES)
-            if form.is_valid():
-                cleaned_data = form.cleaned_data
+            development_form = DevelopmentCreateForm(request.POST, request.FILES)
+            media_form = MediaCreateForm(request.POST, request.FILES)
+            link_form = LinkCreateForm(request.POST)
+
+            media_flag = False
+            if media_form.is_valid():
+                cleaned_data = media_form.cleaned_data
+                media_file = cleaned_data['file']
+                if str(media_file) == 'False':
+                    media.delete()
+                elif cleaned_data['type'] and media_file:
+                    media_flag = True
+                    media.type = cleaned_data['type']
+                    media.file = media_file
+                    media.save()
+
+            link_flag = False
+            if link_form.is_valid():
+                cleaned_data = link_form.cleaned_data
+                if cleaned_data['type'] and cleaned_data['link']:
+                    link_flag = True
+                    link.type = cleaned_data['type']
+                    link.link = cleaned_data['link']
+                    link.save()
+
+            if development_form.is_valid():
+                cleaned_data = development_form.cleaned_data
                 development.title = cleaned_data['title']
                 development.description = cleaned_data['description']
-                development.user = request.user
-                if cleaned_data['top_image']:
-                    development.top_image = cleaned_data['top_image']
+                development_top_image = cleaned_data['top_image']
+                if str(development_top_image) == 'False':
+                    development.top_image.delete()
+                elif development_top_image:
+                    development.top_image = development_top_image
+                development.associations = ','.join(cleaned_data['associations'])
                 development.is_private = cleaned_data['is_private']
+                development.save()
+
+                development.co_developers.clear()
+                for co_developer in development_form.cleaned_data['co_developers']:
+                    development.co_developers.add(co_developer)
+
+                if media_flag:
+                    development.medias.add(media)
+                if link_flag:
+                    development.links.add(link)
+
                 development.updated_at = timezone.now()
                 development.save()
 
